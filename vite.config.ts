7b107fd5a4302687cd64ad20ba742671530a1f61
DakshLohchab <daksh2006gb@gmail.com>
2026-06-12 19:47:09 +0530

    Reapply "fix."
    This reverts commit 469aeee89ce8728a7c97f6dc970ca36865c209c9.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,15 @@ import { resolve } from 'path';
 import fs from 'fs';
 
 export default defineConfig({
+  base: '',
   plugins: [
     react(),
+    {
+      name: 'remove-crossorigin',
+      transformIndexHtml(html) {
+        return html.replace(/ crossorigin/g, '');
+      }
+    },
     {
       name: 'copy-manifest',
       closeBundle() {
